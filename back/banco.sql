3a93b67cb51eddfdcecf700a691192d4e0c94d55
polle <pollymoreira_@hotmail.com>
2025-06-17 16:10:33 -0300

    backend

---
+++ back/banco.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE cinefriends;
+
+use cinefriends;
+
+CREATE TABLE usuario (
+	id int auto_increment primary key,
+    nome varchar (255) not null,
+    genero enum ('Masculino', 'Feminino', 'Não Binário'),
+    data_nascimento date not null,
+    email varchar (255) not null unique,
+    telefone varchar(11) not null,
+    usuario varchar(25) not null,
+    senha varchar(255) not null
+)
+
